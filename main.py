f2ba17ac84590a1334064d72db70b0dc6eba0e04
SlowSpeedChase <58612484+SlowSpeedChase@users.noreply.github.com>
2021-11-30 16:49:11 -0600

    Project creation and running code from another file test!

---
+++ main.py
@@ -0,0 +1,13 @@
+# Simulates a game of hangman
+from header_functions import *
+
+print(test())
+
+
+
+
+# Pick Word
+# Input Letter
+# Letter in Word?
+# Reveal to Player
+# Next Guess or Complete or Hangman
